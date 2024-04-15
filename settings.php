35a7966245e44dfcdbe39ed63c43d479addae1d5
VolcanicMonkey5 <lucasmartiniscool@gmail.com>
2024-04-14 18:06:05 -0700

    Add script for test data population. Revert to first settings file because it seems to work.

--- settings.php
+++ settings.php
@@ -89,7 +89,7 @@ if (true || $connected) { // Ensure $connected is defined and true to display me
 	<p>If you are not a superuser, you should only be able to access the <b>Student Information</b>, <b>Enrollments</b>, <b>Lost Students</b>, and <b>Settings</b> page.
 <?php } ?>
 
-    <form method="post">
+    <form method="post" onsubmit="return validatePassword()">
 		<table class='input'>
 <?php
 	if (!$user_info)
@@ -298,4 +298,3 @@ if (is_superuser($user_info)) {
         }
     }
 </script>
-
