6a9e34bee8dff946d5d42880f765d5ef517bcb50
Tony <sonictony@gmail.com>
2024-04-26 12:42:53 -0700

    Added CSV export functionality for enrollments page and class roster

--- majors.php
+++ majors.php
@@ -1,39 +1,36 @@
+<?php
+ob_start(); // Start output buffering
+
+include_once("_html.php");
+include_once("_sql.php");
+
+if (!get_user_info()) {
+    header("Location: settings.php");
+    exit();
+}
+
+$program_id = extract_int($_GET, 'program_id');
+$roster = [];
+
+if ($program_id != 0) {
+    $roster = get_program_roster($program_id);
+}
+
+$all_programs_blank = array(0 => '') + all_programs();
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2/dist/js/select2.min.js"></script>
     <link rel="stylesheet" type="text/css" href="styles/core.css">
-<?php
-	
-	include_once("_html.php");
-	include_once("_sql.php");
-	
-	if (!get_user_info())
-	{
-		echo("<meta http-equiv='refresh' content='0; url=settings.php' />\n");
-		echo("</head>\n");
-		echo("</html>\n");
-		die();
-	}
-	
-	$program_id = extract_int($_GET, 'program_id');
-    $roster = [];
-	if ($program_id != 0)
-	{
-		$roster = get_program_roster($program_id);
-	}
-	
-	$all_programs_blank = array(0 => '') + all_programs();
-?>
+    <title>Program Roster</title>
+    <link rel='stylesheet' type='text/css' href='styles/_style.css'/>
 </head>
 <body>
 <style>
-
-
     table {
         margin-top: 20px;
         width: 100%;
@@ -56,14 +53,17 @@
         width: 20% !important;
     }
 
+    #exportButton {
+        margin-top: 10px;
+    }
 </style>
 
 <?php
-	echo(messages());
-	echo(linkmenu('Majors'));
+echo(messages());
+echo(linkmenu('Majors'));
 ?>
 <div class="content_container">
-	<form action='majors.php' method='get'>
+    <form action='majors.php' method='get'>
         <td>
             <select id="program_id" name="program_id" class="select2-class">
                 <option value=""></option>
@@ -74,25 +74,25 @@
                 <?php endforeach; ?>
             </select>
         </td>
-		<br />
-		<input type='submit' /> 
-	</form>
-	<table>
-		<tr><th>Name</th><th>CWU ID</th><th>Email</th><th>Advisor</th></tr>
-<?php
-	foreach ($roster as $id => $student_info)
-	{
-		$cwu_id = $student_info['cwu_id'];
-		$name = $student_info['name'];
-		$email = $student_info['email'];
-		$advisor = $student_info['advisor'];
-		echo("\t\t<tr><td style='padding:0px 10px;'><a href='student.php?id=$cwu_id'>$name</a></td><td style='padding:0px 10px;'>$cwu_id</td><td style='padding:0px 10px;'><a href='mailto:$email@cwu.edu'>$email@cwu.edu</a></td><td>$advisor</td></tr>\n");
-	}
-?>
-	</table>
+        <br />
+        <input type='submit' />
+    </form>
+    <button id="exportButton" disabled>Export to CSV</button> <!-- Disabled Export to CSV button -->
+    <table id="rosterTable">
+        <tr><th>Name</th><th>CWU ID</th><th>Email</th><th>Advisor</th></tr>
+        <?php foreach ($roster as $id => $student_info): ?>
+            <tr>
+                <td style='padding:0px 10px;'><a href='student.php?id=<?php echo $student_info['cwu_id']; ?>'><?php echo $student_info['name']; ?></a></td>
+                <td style='padding:0px 10px;'><?php echo $student_info['cwu_id']; ?></td>
+                <td style='padding:0px 10px;'><a href='mailto:<?php echo $student_info['email']; ?>@cwu.edu'><?php echo $student_info['email']; ?>@cwu.edu</a></td>
+                <td><?php echo $student_info['advisor']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </div>
 </body>
 </html>
+
 <script>
     $(document).ready(function() {
         // Initialize the Select2 dropdown
@@ -112,6 +112,24 @@
             var selectedValue = $(this).val();
             localStorage.setItem('selectedProgram', selectedValue);
         });
+
+        // Enable or disable Export to CSV button based on table data availability
+        $('#exportButton').prop('disabled', $('#rosterTable tr').length <= 1);
+        
+        // Export to CSV functionality
+        $('#exportButton').click(function() {
+            var csvContent = "data:text/csv;charset=utf-8,";
+            csvContent += "Last Name, First Name, CWU ID,Email,Advisor\n";
+            <?php foreach ($roster as $id => $student_info): ?>
+                csvContent += "<?php echo $student_info['name']; ?>,<?php echo $student_info['cwu_id']; ?>,<?php echo $student_info['email']; ?>,<?php echo $student_info['advisor']; ?>\n";
+            <?php endforeach; ?>
+
+            var encodedUri = encodeURI(csvContent);
+            var link = document.createElement("a");
+            link.setAttribute("href", encodedUri);
+            link.setAttribute("download", "Program_Roster.csv");
+            document.body.appendChild(link);
+            link.click();
+        });
     });
 </script>
-
--- term.php
+++ term.php
@@ -1,50 +1,36 @@
+<?php
+ob_start(); // Start output buffering
+
+include_once("_html.php");
+include_once("_sql.php");
+
+if (!get_user_info()) {
+    header("Location: settings.php");
+    exit();
+}
+
+$curr_year = date('Y');
+$year = extract_int($_GET, 'year', $curr_year);
+$years = [];
+for ($i = $curr_year - 1; $i < $curr_year + 10; ++$i) {
+    $years[$i] = "$i";
+}
+
+$all_programs_blank = [];
+$all_programs_blank = array(0 => '') + all_programs();
+$program_id = extract_int($_GET, 'program_id');
+
+// Check if the "Export to CSV" button is clicked
+$export_csv = isset($_GET['export_csv']);
+
+if (!$export_csv) {
+    // Output HTML content
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <link rel="stylesheet" type="text/css" href="styles/core.css">
-    <?php
-
-    include_once("_html.php");
-    include_once("_sql.php");
-
-    //! @todo maybe allow anonymous access here since there is no updating
-    if (!get_user_info()) {
-        echo("<meta http-equiv='refresh' content='0; url=settings.php' />\n");
-        echo("</head>\n");
-        echo("</html>\n");
-        die();
-    }
-
-    $curr_year = date('Y');
-    $year = extract_int($_GET, 'year', $curr_year);
-    $years = [];
-    //! @todo should go back to previous year if we are in Winter or Spring
-    for ($i = $curr_year - 1; $i < $curr_year + 10; ++$i) {
-        $years[$i] = "$i";
-    }
-
-    if (isset($_GET['year']) && array_key_exists($_GET['year'], $years)) {
-        $year = $_GET['year'];
-    } else {
-        $year = $curr_year - 1; // Default to the earliest year in the range
-    }
-
-    //array of majors
-    $all_programs_blank = [];
-    $all_programs_blank = array(0 => '') + all_programs();
-    $program_id = extract_int($_GET, 'program_id');
-
-    // Check if program_id is selected in URL parameters
-    if (isset($_GET['program_id']) && array_key_exists($_GET['program_id'], $all_programs_blank)) {
-        $program_id = $_GET['program_id'];
-    } else {
-        // Default to an empty program if no program is selected
-        $program_id = 0;
-    }
-
-
-    ?>
     <title>Enrollments</title>
     <link rel='stylesheet' type='text/css' href='styles/_style.css'/>
 </head>
@@ -54,80 +40,117 @@
 <?php echo(linkmenu("Enrollments")); ?>
 
 <div class="content_container">
-<h1>Enrollments</h1>
+    <h1>Enrollments</h1>
 
-<table>
-    <tr>
-        <td style="vertical-align: top;">
-            <form action='' method='get'>
-                <?php echo(array_menu('Year Starting Fall: ', $years, 'year', $year, true));?>
-                <?php echo "<div style='margin-right: 10px;'></div>";?>
-                <?php echo(array_menu('Program: ', $all_programs_blank, 'program_id',$program_id ,true)); ?>
-            </form>
-        </td>
+    <form action='' method='get'>
+        <?php echo(array_menu('Year Starting Fall: ', $years, 'year', $year, true));?>
+        <?php echo "<div style='margin-right: 10px;'></div>";?>
+        <?php echo(array_menu('Program: ', $all_programs_blank, 'program_id',$program_id ,true)); ?>
+        <input type="submit" name="export_csv" value="Export to CSV"/>
+    </form>
+<?php
+} else {
+    // Output CSV data
+    if ($program_id == 0) {
+        $enrollments = get_enrollments($year);
+    } else {
+        $enrollments = get_enrollments_by_program($year, $program_id);
+    }
 
+    // Set headers for CSV download
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename="Enrollments.csv');
 
-        <?php 
-            if ($program_id == 0){
-                $enrollments = get_enrollments($year);
-            } else {
-                $enrollments = get_enrollments_by_program($year,$program_id);
-            }
-        
-        ?>
+    // Open a file pointer for writing CSV data
+    $output = fopen('php://output', 'w');
 
+    // Write CSV header
+    fputcsv($output, array('Class Name', 'Fall ' . $year, 'Winter ' . ($year + 1), 'Spring ' . ($year + 1), 'Summer ' . ($year + 1)));
 
-
-    </tr>
-</table>
-
-<table>
-    <tr>
-        <td>Class Name</td>
-        <td style='padding:0px 10px;'>Fall <?php echo($year); ?></td>
-        <td style='padding:0px 10px;'>Winter <?php echo($year + 1); ?></td>
-        <td style='padding:0px 10px;'>Spring <?php echo($year + 1); ?></td>
-        <td style='padding:0px 10px;'>Summer <?php echo($year + 1); ?></td>
-    </tr>
-    <?php
+    // Write enrollment data to CSV
     foreach ($enrollments as $class_id => $info) {
         $name = $info['name'];
         $enrollment = $info['enrollment'];
-        $class = '';
-        if ($class == 'even') {
-            $class = 'odd';
-        } else {
-            $class = 'even';
+        $csv_row = array($name);
+        for ($i = 1; $i < 5; ++$i) {
+            $csv_row[] = isset($enrollment[$i]) ? $enrollment[$i] : '';
         }
-        ?>
-        <tr class='<?php echo($class); ?>'>
-            <td><a href='class.php?id=<?php echo($class_id); ?>'><?php echo($name); ?></a></td>
-            <?php
-            for ($i = 1; $i < 5; ++$i) {
-                ?>
-                <td style='text-align:center;'><a
-                            href='roster.php?class_id=<?php echo($class_id); ?>&amp;term=<?php echo($year . $i); ?>'>
-                        <?php
+        fputcsv($output, $csv_row);
+    }
 
-                        if (isset($enrollment[$i])) {
+    // Close the file pointer
+    fclose($output);
 
-                            echo($enrollment[$i]);
-                        } else {
-                            echo(" ");
-                        }
-
-                        ?></a></td>
-                <?php
-            }
-            ?>
+    // Exit script after CSV download
+    exit();
+}
+?>
+    <table>
+        <tr>
+            <td>Class Name</td>
+            <td style='padding:0px 10px;'>Fall <?php echo($year); ?></td>
+            <td style='padding:0px 10px;'>Winter <?php echo($year + 1); ?></td>
+            <td style='padding:0px 10px;'>Spring <?php echo($year + 1); ?></td>
+            <td style='padding:0px 10px;'>Summer <?php echo($year + 1); ?></td>
         </tr>
         <?php
-    
-    }
-    ?>
+        if ($program_id == 0) {
+            $enrollments = get_enrollments($year);
+        } else {
+            $enrollments = get_enrollments_by_program($year, $program_id);
+        }
+        foreach ($enrollments as $class_id => $info) {
+            $name = $info['name'];
+            $enrollment = $info['enrollment'];
+            $class = '';
+            if ($class == 'even') {
+                $class = 'odd';
+            } else {
+                $class = 'even';
+            }
+            ?>
+            <tr class='<?php echo($class); ?>'>
+                <td><a href='class.php?id=<?php echo($class_id); ?>'><?php echo($name); ?></a></td>
+                <?php
+                for ($i = 1; $i < 5; ++$i) {
+                    ?>
+                    <td style='text-align:center;'><a
+                                href='roster.php?class_id=<?php echo($class_id); ?>&amp;term=<?php echo($year . $i); ?>'>
+                            <?php
+
+                            if (isset($enrollment[$i])) {
+
+                                echo($enrollment[$i]);
+                            } else {
+                                echo(" ");
+                            }
+
+                            ?></a></td>
+                    <?php
+                }
+                ?>
+            </tr>
+            <?php
+
+        }
+        ?>
 
 
-</table>
+    </table>
 </div>
+<script>
+    // Disable Export to CSV button if table has no data
+    window.onload = function() {
+        var exportButton = document.getElementsByName("export_csv")[0];
+        var tableRows = document.getElementsByTagName("tr");
+        if (tableRows.length <= 1) {
+            exportButton.disabled = true;
+        }
+    };
+</script>
 </body>
 </html>
+
+<?php
+ob_end_flush(); // Flush output buffer
+?>
