8dfb68e3b13457f457301506b89c35ea8a88fd2b
VolcanicMonkey5 <lucasmartiniscool@gmail.com>
2024-05-27 16:39:11 -0700

    Update enrollments UI

--- term.php
+++ term.php
@@ -32,7 +32,7 @@ if (!$export_csv) {
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <link rel="stylesheet" type="text/css" href="styles/navbar.css">
     <title>Enrollments</title>
-    <link rel='stylesheet' type='text/css' href='styles/_style.css'/>
+    <link rel='stylesheet' type='text/css' href='styles/test.css'/>
 </head>
 <body>
 
@@ -40,13 +40,65 @@ if (!$export_csv) {
 <?php echo(linkmenu("Enrollments")); ?>
 
 <div class="content_container">
-    <h1>Enrollments</h1>
-
-    <form action='' method='get'>
+    <form action='' method='get' class="long">
+        <h1>Enrollments</h1>
         <?php echo(array_menu('Year Starting Fall: ', $years, 'year', $year, true));?>
         <?php echo "<div style='margin-right: 10px;'></div>";?>
         <?php echo(array_menu('Program: ', $all_programs_blank, 'program_id',$program_id ,true)); ?>
         <input type="submit" name="export_csv" value="Export to CSV"/>
+
+        <table>
+            <tr>
+                <td>Class Name</td>
+                <td style='padding:0px 10px;'>Fall <?php echo($year); ?></td>
+                <td style='padding:0px 10px;'>Winter <?php echo($year + 1); ?></td>
+                <td style='padding:0px 10px;'>Spring <?php echo($year + 1); ?></td>
+                <td style='padding:0px 10px;'>Summer <?php echo($year + 1); ?></td>
+            </tr>
+            <?php
+            if ($program_id == 0) {
+                $enrollments = get_enrollments($year);
+            } else {
+                $enrollments = get_enrollments_by_program($year, $program_id);
+            }
+            foreach ($enrollments as $class_id => $info) {
+                $name = $info['name'];
+                $enrollment = $info['enrollment'];
+                $class = '';
+                if ($class == 'even') {
+                    $class = 'odd';
+                } else {
+                    $class = 'even';
+                }
+                ?>
+                <tr class='<?php echo($class); ?>'>
+                    <td><a href='class.php?id=<?php echo($class_id); ?>'><?php echo($name); ?></a></td>
+                    <?php
+                    for ($i = 1; $i < 5; ++$i) {
+                        ?>
+                        <td style='text-align:center;'><a
+                                href='roster.php?class_id=<?php echo($class_id); ?>&amp;term=<?php echo($year . $i); ?>'>
+                                <?php
+
+                                if (isset($enrollment[$i])) {
+
+                                    echo($enrollment[$i]);
+                                } else {
+                                    echo(" ");
+                                }
+
+                                ?></a></td>
+                        <?php
+                    }
+                    ?>
+                </tr>
+                <?php
+
+            }
+            ?>
+
+
+        </table>
     </form>
 <?php
 } else {
@@ -85,58 +137,6 @@ if (!$export_csv) {
     exit();
 }
 ?>
-    <table>
-        <tr>
-            <td>Class Name</td>
-            <td style='padding:0px 10px;'>Fall <?php echo($year); ?></td>
-            <td style='padding:0px 10px;'>Winter <?php echo($year + 1); ?></td>
-            <td style='padding:0px 10px;'>Spring <?php echo($year + 1); ?></td>
-            <td style='padding:0px 10px;'>Summer <?php echo($year + 1); ?></td>
-        </tr>
-        <?php
-        if ($program_id == 0) {
-            $enrollments = get_enrollments($year);
-        } else {
-            $enrollments = get_enrollments_by_program($year, $program_id);
-        }
-        foreach ($enrollments as $class_id => $info) {
-            $name = $info['name'];
-            $enrollment = $info['enrollment'];
-            $class = '';
-            if ($class == 'even') {
-                $class = 'odd';
-            } else {
-                $class = 'even';
-            }
-            ?>
-            <tr class='<?php echo($class); ?>'>
-                <td><a href='class.php?id=<?php echo($class_id); ?>'><?php echo($name); ?></a></td>
-                <?php
-                for ($i = 1; $i < 5; ++$i) {
-                    ?>
-                    <td style='text-align:center;'><a
-                                href='roster.php?class_id=<?php echo($class_id); ?>&amp;term=<?php echo($year . $i); ?>'>
-                            <?php
-
-                            if (isset($enrollment[$i])) {
-
-                                echo($enrollment[$i]);
-                            } else {
-                                echo(" ");
-                            }
-
-                            ?></a></td>
-                    <?php
-                }
-                ?>
-            </tr>
-            <?php
-
-        }
-        ?>
-
-
-    </table>
 </div>
 <script>
     // Disable Export to CSV button if table has no data
@@ -148,6 +148,11 @@ if (!$export_csv) {
         }
     };
 </script>
+    <svg class="wave" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#fdfdfd" fill-opacity="1"
+              d="M0,192L60,170.7C120,149,240,107,360,112C480,117,600,171,720,186.7C840,203,960,181,1080,149.3C1200,117,1320,75,1380,53.3L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+              data-darkreader-inline-fill="" style="--darkreader-inline-fill: #007acc;"></path>
+    </svg>
 </body>
 </html>
 
