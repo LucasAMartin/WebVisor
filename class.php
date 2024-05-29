a7f50ab6a13268ba56d9ebf01e4555536342b248
VolcanicMonkey5 <lucasmartiniscool@gmail.com>
2024-05-29 16:28:21 -0700

    Add max width for select

--- class.php
+++ class.php
@@ -405,10 +405,19 @@ echo(linkmenu('Class Information'));
     </table>
 
 </form>
-
 <?php
 }
 ?>
-
+    <?php
+    if ($class_id == 0) {
+        ?>
+        <svg class="wave" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#fdfdfd" fill-opacity="1"
+                  d="M0,192L60,170.7C120,149,240,107,360,112C480,117,600,171,720,186.7C840,203,960,181,1080,149.3C1200,117,1320,75,1380,53.3L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+                  data-darkreader-inline-fill="" style="--darkreader-inline-fill: #007acc;"></path>
+        </svg>
+        <?php
+    }
+    ?>
 </body>
 </html>
